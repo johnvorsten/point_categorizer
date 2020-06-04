b14bcf786b6dc91868b5e214d2f8ffc653bd7cc7
John Vorsten <vorstenjohn@gmail.com>
2020-06-04 13:08:00 -0500

    Starting work on writing TF Records

--- ranking/Labeling_main.py
+++ ranking/Labeling_main.py
@@ -4,178 +4,53 @@ Created on Sat May 30 16:29:46 2020
 
 @author: z003vrzk
 """
+# Python imports
+
+# Third party imports
+from pymongo.errors import InvalidDocument
+
+# Local imports
+if __name__ == '__main__':
+    # Remove the drive letter on windows
+    _CWD = os.path.splitdrive(os.getcwd())[1]
+    _PARTS = _CWD.split(os.sep)
+    # Project dir is one level above cwd
+    _PROJECT_DIR = os.path.join(os.sep, *_PARTS[:-1])
+    if _PROJECT_DIR not in sys.path:
+        sys.path.insert(0, _PROJECT_DIR)
+
+from ranking import Labeling
+from transform import transform_pipeline
+from extract import extract
+from extract.SQLAlchemyDataDefinition import (Clustering, Points, Netdev, Customers,
+                                              ClusteringHyperparameter)
+from extract.SQLAlchemyDataDefinition import Labeling as SQLTableLabeling
+from clustering.accuracy_visualize import Record, get_records
+
+ExtractLabels = Labeling.ExtractLabels()
 
 #%% Extract labels for all datasets
 
-from pymongo.errors import InvalidDocument
-from JVWork_Mongo import correct_encoding
-from JVWork_Labeling import ExtractLabels
-from JVWork_AccuracyVisual import import_error_dfs
-Extract = ExtractLabels()
+# Set up connection to SQL
+Insert = extract.Insert(server_name='.\\DT_SQLEXPR2008',
+                        driver_name='SQL Server Native Client 10.0',
+                        database_name='Clustering')
 
-# New Database
-db = client['master_points']
-collection = db['raw_databases']
+# Get all records relating to one customer
+customer_id = 15
+sel = sqlalchemy.select([Clustering.id, Clustering.correct_k])\
+    .where(Clustering.customer_id.__eq__(customer_id))
+res = Insert.core_select_execute(sel)
+primary_keys = [x.id for x in res]
+correct_k = res[0].correct_k
 
-#Import your database (.csv)
-csv_file = r'data\master_pts_db.csv'
-sequence_tag = 'DBPath'
+sel = sqlalchemy.select([Customers.name]).where(Customers.id.__eq__(customer_id))
+customer_name = Insert.core_select_execute(sel)[0].name
 
-#Get unique names
-unique_tags = pd.read_csv(csv_file, index_col=0, usecols=['DBPath'])
-unique_tags = list(set(unique_tags.index))
-
-records = import_error_dfs()
-
-for tag in unique_tags:
-
-    #Calculate Features
-    new_labels = {}
-    labels, hyper_dict = Extract.calc_labels(records, tag)
-
-    for key in labels.keys():
-        new_labels[str(key)] = labels[key]
-
-    #Save labels for all datasets in mongo
-    try:
-        collection.update_one({'database_tag':tag}, {'$set':{'hyper_labels':new_labels}})
-    except InvalidDocument:
-        # Python on Windows and Pymongo are not forgiving
-        # If you have foreign data types you have to convert them
-        corrected_dict = correct_encoding(new_labels)
-        collection.update_one({'database_tag':tag},
-                              {'$set':{'hyper_labels':corrected_dict}})
-
-#Make sure everything is tagged
-a = collection.find( {'hyper_labels':{'$exists':False}} )
-
-a = collection.aggregate([
-        {'$match': {'hyper_labels': {'$exists':False} } },
-        {'$group': {'_id': {'my_tag':'$database_tag'},
-                    'obj_id': {'$push':'$_id'} }}])
-
-a = collection.aggregate([
-        {'$group': {'_id':'$database_tag', 'hypers':{'$first':'$hyper_labels'}}},
-        {'$project': {'num_fields':{'$size':{'$objectToArray':'$hypers'}}}},
-        {'$match': {'num_fields':{'$lt':30}}}
-        ])
-
-# This document has no points and causes errors
-collection.delete_one({'database_tag':r'D:\Z - Saved SQL Databases\44OP-263742-AUS_TFC_TRC_Renovation\JobDB.mdf'})
-pass
-
-#%% Save by_size label
-
-#Third party imports
-from pymongo import MongoClient
-
-# Local Imports
-from JVWork_Labeling import choose_best_hyper
-
-# New Database
-client = MongoClient('localhost', 27017)
-db = client['master_points']
-collection = db['raw_databases']
-
-# Goal : choose the best by_size hyperparameter for a database
-
-for db_dict in collection.find():
-    # Extract saved labels
-    labels = db_dict['hyper_labels']
-
-    best_by_size = choose_best_hyper(labels, 'by_size')
-
-    collection.update_one({'_id':db_dict['_id']},
-                          {'$set':{'best_hyper.by_size':best_by_size}})
+# Calculate ranking of all records
+records = get_records(primary_keys)
+best_labels = ExtractLabels.calc_labels(records, correct_k, error_scale=0.8, var_scale=0.2)
 
 
+#%% Get the best clustering hyperparameter for a dataset
 
-a = collection.aggregate([
-        {'$group': {'_id':'$database_tag', 'hypers':{'$first':'$hyper_labels'}}},
-        {'$project': {'num_fields':{'$size':{'$objectToArray':'$hypers'}}}},
-        {'$match': {'num_fields':{'$lt':30}}}
-        ])
-
-b = collection.aggregate([
-        {'$group':{'_id':'$database_tag', 'by_size':{'$first':'$best_hyper.by_size'}}}
-        ])
-pass
-
-for doc in a:
-    print(doc['num_fields'])
-
-for doc in b:
-    print(doc['by_size'])
-
-
-
-#%% Clusterer OR Index
-
-
-for document in collection.find():
-    # Extract saved labels
-    labels = document['hyper_labels']
-
-    ranked_item = []
-    for key, label in labels.items():
-        ranked_item.append(label['index'])
-
-    collection.update_one({'_id':document['_id']},
-                           {'$set':{'best_hyper.index':ranked_item}})
-
-
-#%% n_components
-
-for db_dict in collection.find():
-    # Extract saved labels
-    labels = db_dict['hyper_labels']
-
-    best_n_components = choose_best_hyper(labels, 'n_components')
-
-    collection.update_one({'_id':db_dict['_id']},
-                           {'$set':{'best_hyper.n_components':best_n_components}})
-
-a = collection.aggregate([
-        {'$group': {'_id':'$database_tag', 'hypers':{'$first':'$best_hyper'}}},
-        {'$project': {'num_fields':{'$size':{'$objectToArray':'$hypers'}}}},
-        {'$match': {'num_fields':{'$lt':2}}}
-        ])
-
-b = collection.aggregate([
-        {'$group':{'_id':'$database_tag', 'n_components':{'$first':'$best_hyper.n_components'}}}
-        ])
-pass
-
-for doc in a:
-    print(doc['num_fields'])
-
-for doc in b:
-    print(doc['n_components'])
-
-#%% reduce
-
-for db_dict in collection.find():
-    # Extract saved labels
-    labels = db_dict['hyper_labels']
-
-    best_reduce = choose_best_hyper(labels, 'reduce')
-
-    collection.update_one({'_id':db_dict['_id']},
-                           {'$set':{'best_hyper.reduce':best_reduce}})
-
-a = collection.aggregate([
-        {'$group': {'_id':'$database_tag', 'hypers':{'$first':'$best_hyper'}}},
-        {'$project': {'num_fields':{'$size':{'$objectToArray':'$hypers'}}}},
-        {'$match': {'num_fields':{'$lt':3}}}
-        ])
-
-b = collection.aggregate([
-        {'$group':{'_id':'$database_tag', 'reduce':{'$first':'$best_hyper.reduce'}}}
-        ])
-pass
-
-for doc in a:
-    print(doc['num_fields'])
-
-for doc in b:
-    print(doc['reduce'])
---
+++ ranking/rank_write_record_main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 12:46:14 2020
+
+@author: z003vrzk
+"""
+
