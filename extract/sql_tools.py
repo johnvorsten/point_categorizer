9223a6f558a5415b4becaed80e50018af5090583
John Vorsten <vorstenjohn@gmail.com>
2021-06-12 18:29:38 -0500

    Update SQL tools

--- extract/sql_tools.py
+++ extract/sql_tools.py
@@ -1,22 +1,14 @@
 """
 Created on Thu Mar 28 14:33:12 2019
 
-This module can be used to connect databases to the local SQL server installed
-by default with design tool
+A few utility functions for interacting with MSSQL Server databases
+
 This module has a few important assumptions :
-1) Use the SQL server name DT_SQLEXPR2008
-2) Use trusted connection (windows authentication - not user & password)
-
-Note : Using the format below I do not have to close connections to SQL server
-This is becasue conections are closed after the leave scope (of the with statement)
-conn = pyodbc.connect('DRIVER=MySQL ODBC 5.1 driver;SERVER=localhost;DATABASE=spt;UID=who;PWD=testest')
-with conn:
-    crs = conn.cursor()
-    do_stuff
-
+1) Use trusted connection (windows authentication - not user & password)
 
 @author: z003vrzk
 """
+
 # Python imports
 from datetime import datetime
 
@@ -45,9 +37,6 @@ logging.basicConfig(filename=os.path.join(_log_dir, 'sql_logs.log'),
                     level=logging.DEBUG,
                     format='%(asctime)s %(message)s')
 
-# Global
-DEFAULT_SQL_SERVER_DRIVER = 'SQL+Server+Native+Client+10.0'
-
 
 #%%
 
@@ -57,76 +46,77 @@ class DepreciationError(Exception):
 class NameUsedError(Exception):
     pass
 
-class SQLHandling:
-    r"""A useful class for connecting to SQL servers using microsoft authentication
-    assuming the standard connection properties for the design-tool product
-    by Siemens, inc.
-    Assumes
-    a) Windows/domain authentication
-    b) Server name is configurable, but defaults to '.\DT_SQLEXPR2008' if not
-        defined
-    c) Driver is '{SQL Server Native Client 10.0}' if not defined
-    d) path to log file is configurable, but assumes JobDB_Log.ldf if not defined
-
-    Usage example
-    server_name = r'.\DT_SQLEXPR2008'
-    driver_name = 'SQL Server Native Client 10.0'
-    path_mdf = r'C:\Users\z003vrzk\.spyder-py3\Scripts\Work\PanelBuilder\SQLTest\JobDB.mdf'
-    database_name = 'PBJobDB'
-
-    # Instantiate a class which creates a master connection (to master db)
-    mysql = SQLHandling(server_name=server_name, driver_name=driver_name)
-
-    # If connecting to a networked database turn on flag 1807
-    path_is_net_drive = not os.path.splitdrive(path_mdf)[0] in ['C:', 'D:']
-    path_is_network_name = os.path.splitdrive(path_mdf)[0].startswith(r'\\')
-    if any((path_is_net_drive, path_is_network_name)):
-        mysql.traceon1807(True)
-
-    # Attach a database
-    mysql.attach_database(path_mdf, database_name)
-    mysql.traceon1807(False)
+class SQLBase:
 
 
-    # Read a table
-    df = mysql.read_table(database_name, table_name='POINTBAS')
-
-    # Detach database when you are done
-    mysql.detach_database(database_name) #only detaches PBJobDB currently
-
-    # Close cursors when done
-    connection.close()
-    cursor.close()"""
-
-    def __init__(self, server_name=None, driver_name=None):
+    def __init__(self, server_name, driver_name):
         """A helper class for sql databases. This incldues attaching, detaching,
         and connecting to databases with an sql server. This method only
         supports microsoft authentication (not user and password)
         inputs
         -------
-        server_name : (str) name of sql server. Defaults to the local computer
-        name if no name is given
-        driver : (str) type of driver. Default is {SQL Server Natie Client 10.0}.
-        If no driver is input, then this will be used"""
+        server_name : (str) name of sql server
+        driver_name : (str) type of driver"""
 
         if server_name is None:
-            self.server_name = '.\DT_SQLEXPR2008'
+            msg='server_name cannot be {}, try ".\DT_SQLEXPR2008" or ".\DT_SQLEXPRESS"'
+            raise(ValueError(msg.format(server_name)))
         else:
             self.server_name = server_name
 
         if driver_name is None:
-            self.driver_name = '{SQL Server Native Client 10.0}'
+            driver_1 = "{SQL Server Native Client 10.0}"
+            driver_2 = "{SQL Server Native Client 11.0}"
+            msg='driver_name cannot be {}, try {} or {}'
+            raise(ValueError(msg.format(driver_name, driver_1, driver_2)))
         else:
             self.driver_name = '{{{driver_name}}}'.format(driver_name=driver_name)
 
-        # For pyodbc connection only
-        self.master_connection_str= 'DRIVER={}; SERVER={}; DATABASE=master; Trusted_Connection=yes;'.format(self.driver_name, self.server_name)
+        self._init_master_connection()
 
         return None
 
+
+    def _init_master_connection(self):
+        """Initialize a master connection on startup to test SQL Server
+        connectivity"""
+        if 'master_connection_str' not in self.__dict__:
+            self._set_pyodbc_master_connection_str()
+
+        try:
+            self.master_connection = pyodbc.connect(self.master_connection_str)
+        except Exception as e:
+            logging.debug(e)
+            raise(e)
+        return None
+
+
+    def init_database_connection(self, database_name):
+        """Initialize a database connection"""
+        connection_str = self.get_pyodbc_database_connection_str(database_name)
+        try:
+            self.database_connection = pyodbc.connect(connection_str)
+        except Exception as e:
+            logging.debug(e)
+            raise(e)
+        return None
+
+
     def get_pyodbc_master_connection_str(self):
         """Return the master database connection string"""
+
+        if 'master_connection_str' not in self.__dict__:
+            self._set_pyodbc_master_connection_str()
+
         return self.master_connection_str
+
+
+    def _set_pyodbc_master_connection_str(self):
+        """Set the master database connection string"""
+        pyodbc_base='DRIVER={}; SERVER={}; DATABASE=master; Trusted_Connection=yes;'
+        self.master_connection_str = pyodbc_base.format(self.driver_name, self.server_name)
+        return self.master_connection_str
+
 
     def get_pyodbc_database_connection_str(self, database_name):
         """Return a database specific connection string
@@ -145,12 +135,12 @@ class SQLHandling:
 
         return connection_string
 
-    def get_sqlalchemy_connection_str(self, 
-                                      database_name,
-                                      driver_name=DEFAULT_SQL_SERVER_DRIVER):
-        """engine for sqlalchemy"""
+
+    def get_sqlalchemy_connection_str(self, database_name):
+        """Engine connection string for SQL Alchemy"""
 
         server_name = self.server_name
+        driver_name = self.driver_name
 
         """ Example
         conn_str = 'mssql://.\\DT_SQLEXPR2008/PBJobDB?trusted_connection=yes&driver=SQL+Server+Native+Client+10.0'
@@ -166,8 +156,8 @@ class SQLHandling:
 
     def attach_database(self,
                         path_mdf,
-                        database_name,
-                        path_ldf):
+                        path_ldf,
+                        database_name):
         """Used to attach 'database_name' to the sql server. If path_ldf is
         not defined, then this method assumes the log file is in the same
         directory as path_mdf, with the file name 'JobDB_Log.ldf'. Define
@@ -175,28 +165,24 @@ class SQLHandling:
 
         inputs
         -------
-        path_mdf : (str or Path) user specified path to .MDF file. LDF file
+        path_mdf : (str) user specified path to .MDF file. LDF file
         must be in same directory.
+        path_ldf : (str or Path) path to database log file
         database_name : (str) name of database to attach. This can be any user
             specified name, but it must be used to attach, detach, and connect to
             a specific database
-        path_ldf : (str or Path) path to database log file
-
         output
         -------
         database_name : (str) of actual database name attached as
         """
-        if isinstance(path_mdf, str):
-            path_mdf = Path(path_mdf)
-        elif isinstance(path_mdf, Path):
-            pass
-        else:
-            raise ValueError("path_mdf must be of type str or pathlib.Path." +
-                             " Value passed was {}".format(type(path_mdf)))
+
+        # Check if database connection is already instantiated
+        if 'master_connection' not in self.__dict__:
+            self._init_master_connection()
 
         try:
-            # Check if the selected file is already in use or if the requested
-            # database name is already used
+            """Check if the selected file is already in use or if the requested
+            # database name is already used"""
             (file_used_bool,
              name_used_bool,
              existing_database_name) = self.check_existing_database(path_mdf, database_name)
@@ -220,26 +206,28 @@ class SQLHandling:
 
                 raise NameUsedError(msg)
 
-
             # Flag 1807 must be ON to connect remote databases
             path_is_net_drive = not os.path.splitdrive(path_mdf)[0] in ['C:', 'D:']
             path_is_network_name = os.path.splitdrive(path_mdf)[0].startswith('\\')
             if any((path_is_net_drive, path_is_network_name)):
                 self.traceon1807(True)
 
-            sql1 = "CREATE DATABASE [{}]".format(database_name)
-            sql2 = "ON (Filename = '{pathMDF}'), (Filename = '{pathLDF}')"\
-                    .format(pathMDF=path_mdf, pathLDF=path_ldf)
-            sql3 = "FOR Attach"
-            sql = sql1 + " " + sql2 + " " + sql3
+            _dat_name = database_name + '_dat'
+            _log_name = database_name + '_log'
+            sql = """CREATE DATABASE [{database_name}]
+                    ON PRIMARY (NAME={_dat_name}, FILENAME='{path_mdf}')
+                    LOG ON (NAME={_log_name}, FILENAME='{path_ldf}')
+                    FOR ATTACH WITH FILESTREAM (DIRECTORY_NAME='Test');
+                    """.format(database_name=database_name,
+                    path_mdf=path_mdf,
+                    path_ldf=path_ldf,
+                    _dat_name=_dat_name,
+                    _log_name=_log_name)
 
-            # Connections are closed after sql is committed
-            with pyodbc.connect(self.master_connection_str) as connectionMaster:
-                cursorMaster = connectionMaster.cursor()
-                connectionMaster.autocommit = True
-                cursorMaster.execute(sql)
-                # connectionMaster.autocommit = False
-                cursorMaster.close()
+            self.master_connection.autocommit = True
+            with self.master_connection.cursor() as master_cursor:
+                master_cursor.execute(sql)
+            self.master_connection.autocommit = False
 
             logging.info('Database : {} connected'.format(database_name))
 
@@ -250,7 +238,8 @@ class SQLHandling:
             logging.debug(e)
             raise(e)
 
-        return database_name
+        return None
+
 
     def detach_database(self, database_name):
         """Used to detach database_name.  Use
@@ -278,7 +267,7 @@ class SQLHandling:
         subprocess.call(['sqlcmd', '-S', self.server_name, '-i', detach_file])
         logging.info('Database {} removed'.format(database_name))
 
-        return
+        return None
 
 
     def check_existing_database(self, path_mdf, database_name):
@@ -290,110 +279,131 @@ class SQLHandling:
 
         inputs
         -------
-        path_mdf : (str or pathlib.Path) path to master data file .mdf
+        path_mdf : (str) path to master data file .mdf
         database_name : (str) name of database you will try to connect as.
         It is the logical name of the database
-
         ouputs
         -------
         (file_used_bool, name_used_bool, existing_database_name)
         file_used_bool : (bool) True if the operating system file is already
-        attached by the server
+            attached by the server
         name_used_bool : (bool) True if the database name requested is already
-        in use by another database
+            in use by another database
         existing_database_name : (str) name of existing database if
-        file_used_bool is true"""
-
-        path_mdf = Path(path_mdf)
+            file_used_bool is true
+        """
 
         sql = """select [name] as logical_name, physical_name,
         	(select name
         	from [master].[sys].[databases] as t2
         	where t2.database_id = t1.database_id) as database_name
         from sys.master_files as t1"""
-        with pyodbc.connect(self.master_connection_str) as connectionMaster:
-            # Find all database currently connected
-            cursorMaster = connectionMaster.cursor()
-            cursorMaster.execute(sql)
-            rows = cursorMaster.fetchall()
-            cursorMaster.close()
+        with self.master_connection.cursor() as master_cursor:
+            master_cursor.execute(sql)
+            rows = master_cursor.fetchall()
 
         # Assume the file and database name is not initially used
         file_used_bool = False
         name_used_bool = False
         existing_database_name = None
 
-        # If the query indicates the .mdf file name OR database name is already
-        # Used then change the boolean state
+        """If the query indicates the .mdf file name OR database name is already
+        Used then change the boolean state"""
         for row in rows:
-            if Path(row.physical_name) == path_mdf:
+            if self.path_equal(row.physical_name, path_mdf):
                 file_used_bool = True
                 existing_database_name = row.database_name
             if row.database_name == database_name:
                 name_used_bool = True
+                existing_database_name = row.database_name
 
         return (file_used_bool, name_used_bool, existing_database_name)
 
-    def read_table(self, database_name, table_name):
-        """Read a table to dataframe using pyodbc and pandas. The server and driver
-        used to instantiate the class is used (self.server_name, self.driver_name)
+    @staticmethod
+    def path_equal(path1, path2):
+        """Test path equality cross operating system. This is required instead
+        of os.path.samefile because SQL .mdf files (especially the master)
+        cannot be operated on and file properties examined
         inputs
         -------
-        database_name : (str) name of database to read from
-        table_name : (str) name of table in database_name to return
-        outputs
+        path1 : (str) path like file name
+        path2 : (str) path like file name
+        returns
         -------
-        df : (pandas.DataFrame) SQL table"""
+        (bool) True if paths are the same"""
 
-        driver_name = self.driver_name
-        server_name = self.server_name
+        path1 = os.path.normpath(os.path.normcase(path1))
+        path2 = os.path.normpath(os.path.normcase(path2))
 
-        # For pyodbc connection only
-        pydobc_conn_str= 'DRIVER={}; SERVER={}; DATABASE={}; Trusted_Connection=yes;'\
-                                .format(driver_name,
-                                        server_name,
-                                        database_name)
-        sql_query = """SELECT * FROM {}""".format(table_name)
+        return path1 == path2
 
-        try:
-            with pyodbc.connect(pydobc_conn_str) as connection:
-                df = pd.read_sql(sql_query, con=connection)
-        except Exception as e:
-            logging.info(e)
-            raise(e)
-
-        return df
-
-    def pandas_read_sql(self,
-                        sql_query,
-                        database_name):
+    def pandas_execute_sql(self, sql_query):
         """Read a table to dataframe using pyodbc and pandas. The server and driver
         used to instantiate the class is used (self.server_name, self.driver_name)
         inputs
         -------
         sql_query : (str) sql string to execute
-        database_name : (str) name of database to read from
-        table_name : (str) name of table in database_name to return
         outputs
         -------
         df : (pandas.DataFrame) SQL table"""
 
-        driver_name = self.driver_name
-        server_name = self.server_name
+        if not 'database_connection' in self.__dict__:
+            msg='No database connection initialized. Try self.init_database_connection'
+            raise NameError(msg)
 
         # For pyodbc connection only
-        pydobc_conn_str= 'DRIVER={}; SERVER={}; DATABASE={}; Trusted_Connection=yes;'\
-                                .format(driver_name,
-                                        server_name,
-                                        database_name)
         try:
-            with pyodbc.connect(pydobc_conn_str) as connection:
-                df = pd.read_sql(sql_query, con=connection)
+            with self.database_connection as connection:
+                df = pd.read_sql(sql_query, connection)
         except Exception as e:
-            logging.info(e)
+            logging.debug(e)
             raise(e)
 
         return df
+
+    def execute_sql(self, sql_query):
+        """Execute a SQL statement and return rows
+        inputs
+        -------
+        sql_query : (str) sql string to execute
+        outputs
+        -------
+        rows : ()
+        """
+        if not 'database_connection' in self.__dict__:
+            msg='No database connection initialized. Try self.init_database_connection'
+            raise NameError(msg)
+
+        try:
+            with self.database_connection.cursor() as cursor:
+                cursor.execute(sql_query)
+                rows = cursor.fetchall()
+        except Exception as e:
+            logging.debug(e)
+            raise(e)
+
+        return rows
+
+    def execute_sql_master(self, sql_query):
+        """Execute a SQL statement against system databases only
+        (uses the master database)
+        inputs
+        -------
+        sql_query : (str) sql string to execute
+        outputs
+        -------
+        rows : (list) of
+        """
+
+        try:
+            with self.master_connection.cursor() as cursor:
+                cursor.execute(sql_query)
+                rows = cursor.fetchall()
+        except Exception as e:
+            logging.debug(e)
+            raise(e)
+
+        return rows
 
     def get_UNC(self):
         """Return a users mapped network drives. UNC path will be used for
@@ -415,7 +425,7 @@ class SQLHandling:
             if output.__contains__(letter + ':'):
                 drives.append(letter)
 
-        #get UNC server names
+        # Get UNC server names
         output = output.splitlines()
         serverUNC = []
         for lines in output:
@@ -425,6 +435,7 @@ class SQLHandling:
         for index, letter in enumerate(drives):
             myOutput[letter] = serverUNC[index]
         return myOutput
+
 
     def traceon1807(self, Flag):
         """Turn on/off Trace Flag 1807 based on user input True or False
@@ -436,11 +447,8 @@ class SQLHandling:
         else:
             sql = """DBCC TRACEOFF(1807)"""
 
-        with pyodbc.connect(self.master_connection_str) as connectionMaster:
-            cursorMaster = connectionMaster.cursor()
-            cursorMaster.execute(sql)
-            cursorMaster.commit()
-            cursorMaster.close()
+        with self.master_connection.cursor() as master_cursor:
+            master_cursor.execute(sql)
+            master_cursor.commit()
 
         return None
-
